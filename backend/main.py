1cd74c233460a15ec20a089ebd92d55fc49bf149
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2025-06-24 11:53:30 +0200

    update

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+from core.config import logger, settings
 
 app = FastAPI()
+mongodb_client = None
+db = None
+
+@app.on_event("startup")
+async def startup_db_client():
+    global mongodb_client, db
+    logger.info("Connecting to MongoDB...")
+    mongodb_client = AsyncIOMotorClient(settings.MONGO_URI)
+    db = mongodb_client[settings.MONGO_DB]
+    logger.info("Connected to MongoDB.")
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    global mongodb_client
+    logger.info("Disconnecting from MongoDB...")
+    mongodb_client.close()
+    logger.info("Disconnected from MongoDB.")
 
 @app.get("/")
-def read_root():
+async def read_root():
+    logger.info("Route / called")
     return {"message": "Hello World"}       
 
