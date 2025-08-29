d614f43dd0155adc9da0bd11a21e136451fd9946
Sugethan <gunaratnamsugethan@gmail.com>
2025-08-30 00:21:21 +0200

    feat: chat route

--- backend/core/utils/eleven_labs.py
+++ backend/core/utils/eleven_labs.py
@@ -1,14 +1,22 @@
-import requests
+import httpx
+from core.config import logger
 from core.config import settings
 
 
-def text_to_speech_file(text: str) -> bytes:
-    response = requests.post(
-        url=settings.TTS_URL,
-        headers={
-            "xi-api-key": settings.ELEVENLABS_API_KEY,
-        },
-        json={"text": text, "model_id": "eleven_multilingual_v2"},
-    )
+async def text_to_speech(text: str) -> bytes:
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(
+                url=settings.TTS_URL,
+                headers={
+                    "xi-api-key": settings.ELEVENLABS_API_KEY,
+                },
+                json={"text": text, "model_id": "eleven_multilingual_v2"},
+            )
 
-    return response.content
+            response.raise_for_status()
+
+        except Exception as e:
+            logger.error(e)
+
+        return response.content
